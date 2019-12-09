96533513739532823307c3271ec0072dedd2d5cd
italiy <vit47903@gmail.com>
2019-12-09 23:57:40 +0200

    first commit

---
+++ app/src/main/java/com/example/tabtabexperiment/PagerAdapter.java
@@ -0,0 +1,29 @@
+package com.example.tabtabexperiment;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+public class PagerAdapter extends FragmentStatePagerAdapter {
+    private final int NumOfTabs;
+
+    public PagerAdapter(FragmentManager fm, int NumOfTabs) {
+        super(fm);
+        this.NumOfTabs = NumOfTabs;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0: return new TabFragment1();
+            case 1: return new TabFragment2();
+            case 2: return new TabFragment3();
+            default:return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return NumOfTabs;
+    }
+}
